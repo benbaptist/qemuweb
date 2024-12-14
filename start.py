4ac7caa5f78c939c4a21ce1efb954816f6298ca9
Ben Baptist <sawham6@mgail.com>
2024-12-14 11:42:43 -0500

    quick comment about what start.py is

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Alternative way to start the application, without installing the package and using the 'qemuweb' command
+
 from qemuweb.cli import run
 
 if __name__ == '__main__':
