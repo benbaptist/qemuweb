d53b9c57e86f3fedb6caed167ddd9f30ac113af4
Ben Baptist <sawham6@mgail.com>
2024-12-14 11:37:58 -0500

    simplified start.py, added argument parsing with click

--- qemuweb/cli.py
+++ qemuweb/cli.py
@@ -1,1 +1,29 @@
- 
+import click
+import eventlet
+eventlet.monkey_patch()
+
+from .web.app import create_app, socketio
+from .config.manager import config, DEFAULT_CONFIG
+
+@click.command()
+@click.option('--host', default=None, help='Host to bind to')
+@click.option('--port', default=None, type=int, help='Port to bind to')
+@click.option('--debug/--no-debug', default=None, help='Enable debug mode')
+def run(host, port, debug):
+    """Run the QEMU Web Interface"""
+    app = create_app()
+    
+    # Override config with CLI arguments if provided
+    web_config = config['web_interface'].copy()
+    if host is not None:
+        web_config['host'] = host
+    if port is not None:
+        web_config['port'] = port
+    if debug is not None:
+        web_config['debug'] = debug
+    
+    socketio.run(app,
+                host=web_config['host'],
+                port=web_config['port'],
+                debug=web_config['debug'],
+                use_reloader=False) 
